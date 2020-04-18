517ace01d80c347064f3e02e456a963487a0c704
ghl2020 <63026155+ghl2020@users.noreply.github.com>
2020-04-18 16:29:10 -0500

    Add files via upload

---
+++ src/utn.h
@@ -0,0 +1,20 @@
+/*
+ * utn.h
+ *
+ *  Created on: 6 abr. 2020
+ *      Author: teste
+ */
+
+#ifndef UTN_H_
+#define UTN_H_
+
+
+
+#endif /* UTN_H_ */
+
+
+int getInt(char mensaje[]);
+float getFloat(char mensaje[]);
+char getChar(char mensaje[]);
+
+char getNumeroAleatorio(int desde , int hasta, int iniciar);
